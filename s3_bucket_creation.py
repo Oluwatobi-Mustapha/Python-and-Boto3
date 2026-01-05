df60850d9c1df61a6ea649129ab7a2823f8643c2
Oluwatobi Mustapha <oluwatobimustapha539@gmail.com>
2026-01-05 18:49:47 +0100

    Update S3 bucket creation script with error handling & comments

--- s3_bucket_creation.py
+++ s3_bucket_creation.py
@@ -1,24 +1,24 @@
 import boto3
-from botocore.exceptions import ClientError #This is for Error handling
+from botocore.exceptions import ClientError # This is for Error handling
 
-s3_client = boto3.client('s3')
+s3 = boto3.resource('s3')
 
 bucket_name = 'google'
 
 try: 
-    s3_client.create_bucket(Bucket=bucket_name) # For creating the bucket
+    s3.create_bucket(Bucket=bucket_name) # For creating the bucket
 
-    print(f"Your bucket has been successfully created: {bucket_name}")
+    print(f"Congrats! Your bucket has been successfully created: {bucket_name}")
   
 except ClientError as e:
 
-    error_code = e.response['Error']['Code']
+    error_code = e.response['Error']['Code'] # This helps us checks for what kinds of error occurred
 
     if error_code == 'BucketAlreadyOwnedByYou':
-        print(f" Note: This bucket is already owned by you {bucket_name}")
+        print(f"Note: This bucket is already owned by you {bucket_name}")
 
     elif error_code == 'BucketAlreadyExists':
-        print(f"Unfortunately, bucket: {bucket_name} already exists globally")
+        print(f"Unfortunately, The bucket '{bucket_name}' already exists globally")
 
     else:
         print(f"Unknown Error {e}")
