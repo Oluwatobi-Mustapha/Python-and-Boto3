683292b57111cbf1120362801c08efa0ffd8c45d
Oluwatobi Mustapha <oluwatobimustapha539@gmail.com>
2026-01-09 20:59:01 +0100

    Add idempotent EC2 script that toggles between create and terminate

---
+++ aws_ec2_toggle.py
@@ -0,0 +1,53 @@
+import boto3
+from botocore.exceptions import ClientError
+
+# Setup connection
+ec2 = boto3.client('ec2', region_name='eu-north-1')
+instance_name = 'practical-ec2'
+
+# CHECK: Does the server exist?
+# We only care about running/stopped instances, not terminated ones
+print(f"Checking for existing '{instance_name}'...")
+
+response = ec2.describe_instances(
+    Filters=[
+        {'Name': 'tag:Name', 'Values': [instance_name]},
+        {'Name': 'instance-state-name', 'Values': ['running', 'pending', 'stopped']}
+    ]
+)
+
+# TOGGLE LOGIC: If found, delete it. If not, create it.
+if response['Reservations']:
+    # Get the ID of the instance we just found
+    instance_id = response['Reservations'][0]['Instances'][0]['InstanceId']
+    
+    print(f"Instance already exists ({instance_id}). Terminating it...")
+
+    ec2.terminate_instances(InstanceIds=[instance_id])
+    
+    print("Termination successful. Exiting.")
+    exit() 
+
+# CREATE: If we get here, it means no instance was found
+print("No duplicates found. Creating new instance...")
+
+try:
+    response = ec2.run_instances(  
+        ImageId='ami-05957b13c4a38c156', # Amazon Linux 2023 (Stockholm)
+        MinCount=1,
+        MaxCount=1,
+        InstanceType='t3.micro', # t2.micro is not available in eu-north-1
+        KeyName='boto3', # If you are cloning this, pls change this to a KeyPair you own!          
+        TagSpecifications=[
+            {
+                'ResourceType': 'instance',
+                'Tags': [{'Key': 'Name', 'Value': instance_name}]
+            },
+        ]
+    )
+    # Print the new ID so we know it worked
+    new_id = response['Instances'][0]['InstanceId']
+    print(f"Success! Created instance: {new_id}")
+        
+except Exception as e:
+    print(f"Failed to create instance: {e}")
