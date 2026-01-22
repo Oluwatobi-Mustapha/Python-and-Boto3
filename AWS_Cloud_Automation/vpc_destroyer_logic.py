a08cd3b49b984cafecb3093510296875f9d50679
Oluwatobi Mustapha <oluwatobimustapha539@gmail.com>
2026-01-22 19:25:11 +0100

    update script to delete multiple subnets and also check  if VPC exist at all before running

--- AWS_Cloud_Automation/vpc_destroyer_logic.py
+++ AWS_Cloud_Automation/vpc_destroyer_logic.py
@@ -3,10 +3,20 @@ from botocore.exceptions import ClientError
 
 # Configuration
 VPC_CIDR_BLOCK = '10.0.0.0/16'
-SUBNET_CIDR_BLOCK = '10.0.1.0/24'
 SECURITY_GROUP_NAME = 'Boto3-sg'
 
 client = boto3.client('ec2')
+# 0. Find the VPC
+vpc_response = client.describe_vpcs(
+    Filters=[{'Name': 'cidr-block', 'Values':[VPC_CIDR_BLOCK]}]
+)
+if not vpc_response['Vpcs']:
+    print("VPC not found. Exiting.")
+    exit()
+
+vpc_id = vpc_response['Vpcs'][0]['VpcId']
+print(f"Found VPC: {vpc_id}")
+
 
 # 1. find and delete security group
 try:
@@ -22,20 +32,18 @@ except ClientError as e:
     print(f"Error deleting SG: {e}")
 
 # Step 2. Find and Delete subnet
-# We need to find the subnet ID using its CIDR block before we can delete it.
+# find the subnet ID using its CIDR block before deletion.
 
-subnet_response = client.describe_subnets(
-    Filters=[{'Name': 'cidr-block', 'Values': [SUBNET_CIDR_BLOCK]},]
+# Find all subnets in the VPC
+subnets_response = client.describe_subnets(
+    Filters=[{'Name': 'vpc-id', 'Values': [vpc_id]}]
 )
-if subnet_response['Subnets']:
-    # Access the first subnet in the list, then grab its ID
-    subnet_id = subnet_response['Subnets'][0]['SubnetId']
+for subnet in subnets_response['Subnets']:
+    subnet_id = subnet['SubnetId']
     client.delete_subnet(SubnetId=subnet_id)
     print(f"Deleted Subnet: {subnet_id}")
 
-
 # 3. Find the VPC id to detach igw 
-
 vpc_response = client.describe_vpcs(
     Filters=[{'Name': 'cidr', 'Values':[VPC_CIDR_BLOCK]}]
 )
@@ -45,29 +53,14 @@ if vpc_response['Vpcs']:
 
 #4. Detach igw 
 igw_response = client.describe_internet_gateways(
-   Filters=[{'Name': 'attachment.vpc-id', 'Values': [vpc_id]},] # Use the ID we just found
+   Filters=[{'Name': 'attachment.vpc-id', 'Values': [vpc_id]}] # Use the ID we just found
 )
-
-if igw_response['InternetGateways']:
-    igw_id = igw_response['InternetGateways'][0]['InternetGatewayId']
-    print(igw_id)
-
-    # Now we have the IGW ID
-
-# My personal Note from doc: Detaches an internet gateway from a VPC, disabling connectivity between the internet and the VPC.
-# The VPC must not contain any running instances with Elastic IP addresses or public IPv4 addresses.
-
-# Step A: Detach it
-    igwd_response = client.detach_internet_gateway(
-        InternetGatewayId=igw_id,
-        VpcId=vpc_id
-    )
-    print(f"Detached IGW: {igw_id} from {vpc_id}")
-
-    # Step B: Delete it
+#  Detach and Delete it
+for igw in igw_response['InternetGateways']:
+    igw_id = igw['InternetGatewayId']
+    client.detach_internet_gateway(InternetGatewayId=igw_id, VpcId=vpc_id)
     client.delete_internet_gateway(InternetGatewayId=igw_id)
-
-    print(f"Deleted IGW: {igw_id}")
+    print(f"Detached and deleted IGW: {igw_id}")    
 
 # 5. CleanUp custom routables associate with my custom vpc
 rt_response = client.describe_route_tables(
@@ -87,7 +80,6 @@ if rt_response['RouteTables']:
             # Now we can delete it
             rt_response = client.delete_route_table( RouteTableId= custom_rt_id)
             print(f"Deleted custom Route Table: {custom_rt_id}")
-
 
 # Step 6. Deleting the Container(Vpc)
 vpc_response = client.delete_vpc(
