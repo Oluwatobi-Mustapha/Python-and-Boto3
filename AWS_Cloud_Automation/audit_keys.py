602ce77c0c57f7c60990f62b699053ccc0d5ee64
Oluwatobi Mustapha <oluwatobimustapha539@gmail.com>
2026-01-26 22:27:35 +0100

    feat: add a deactivation script to the audit.py code

--- AWS_Cloud_Automation/audit_keys.py
+++ AWS_Cloud_Automation/audit_keys.py
@@ -2,26 +2,39 @@ import boto3
 
 client = boto3.client('iam')
 
-# Step 0. Lists the IAM users
+# 0. lists the IAM users
 iam_response = client.list_users(
     MaxItems=20  
 )
 users = iam_response
 
-# Step 1. Iterate over users
+# 1. iterate over users
 for user in users.get('Users', []):
     username = user.get('UserName')
 
-    # Step 2: List access keys
+    # 2: list access keys
     keys_response = client.list_access_keys(UserName=username)
     print(f"\nKeys for {username}: {keys_response['AccessKeyMetadata']}")
 
-    # Step 3. Look for active keys
-    active_count = 0
+    # 3. look for active keys
+    active_keys_list = []
     for key in keys_response['AccessKeyMetadata']:
         if key['Status'] == 'Active':
-            active_count+=1
-        # Step 4. Check the total
-    if active_count>1:
-        print(f"\nuser {username} has {active_count} active_keys")
-    
+            active_keys_list.append(key)
+
+        # 4. sort to find the oldest one (The "Logic")
+        # This sorts by date so index [0] is the oldest
+    if len(active_keys_list) > 1:
+        print(f"\nuser {username} has {len(active_keys_list)} active_keys")
+
+        active_keys_list.sort(key=lambda k: k['CreateDate'])
+        key_to_kill = active_keys_list[0]
+        print(f"Deactivating key: {key_to_kill['AccessKeyId']}")
+
+        # 5. deactivate the key before deletion
+        client.update_access_key(
+             UserName=username, 
+             AccessKeyId= key_to_kill['AccessKeyId'],
+             Status='Inactive'
+        )
+        print(f"SUCCESS: Deactivated key {key_to_kill['AccessKeyId']} for user {username}.")
